7a05b6746876d0bd0dd92a1f89bdc0d5b020e632
Kevin Fechner <kevin@mokka-merch.com>
2023-07-04 16:32:12 +0200

    feat: image proxy working

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-import ipx from "ipx";
-// import { createIPX, createIPXMiddleware } from "ipx";
+import { createIPX, createIPXMiddleware } from "ipx";
 import express from "express";
 import path from "path";
 import * as url from "url";
@@ -9,30 +8,18 @@ const HOSTNAME = "localhost";
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const app = express();
 
-const ipx = createIPX({ format: "webp", id: "test" });
-// app.use(
-//   ipx({
-//     baseUrl: "/image", // Base URL for the image proxy
-//     // domains: ['example.com'], // Allowed image domains (optional)
-//     cache: {
-//       engine: "memory", // Cache engine (memory, redis, or custom)
-//       options: {
-//         max: 500, // Maximum number of images to cache in memory (optional)
-//       },
-//     },
-//   })
-// );
-app.use("/image", createIPXMiddleware(ipx));
-// app.use("/static", express.static(path.join(__dirname, "static")));
-app.use(express.static(path.join(__dirname, "static")));
-
-app.get("/image/:url", (req, res) => {
-  const { url } = req.params;
-  const imageRequest = ipx.proxy(url);
-
-  // Stream the image response from ipx to the client
-  imageRequest.pipe(res);
+const ipx = createIPX({
+  dir: path.resolve(__dirname, "image"),
+  domains: ["localhost:80"],
 });
+
+// http://localhost:80/image/f_webp/test.jpeg
+// http://localhost:80/image/f_webp/test.jpeg
+// http://localhost:80/image/static/test.jpeg
+// http://localhost:80/image/f_webp,w_200,blur_5/test.jpeg
+
+app.use("/image", createIPXMiddleware(ipx));
+app.use(express.static(path.join(__dirname, "static")));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -41,22 +28,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, HOSTNAME, () => {
   console.log(`server listening on: http://${HOSTNAME}:${PORT}`);
 });
-
-// http://localhost:80/f_webp/static/test.jpeg
-// http://localhost:80/_/static/test.jpeg
-// http://localhost:80/image/test.jpeg
-// http://localhost:80/image/static/test.jpeg
-// http://localhost:80/static/test.jpeg
-// http://localhost:80/image/test.jpeg
-// http://localhost:80/image/width_200/test.jpeg
-// http://localhost:80/image/test.jpeg
-// http://localhost:80/image/test.jpeg
-// Resource id is missing
-// working:
-// app.use("/static", express.static(path.join(__dirname, "static")));
-
-// IPX Error: Error: Modifiers are missing (/)
-
-// ---
-// http://localhost/image/#fii/test.jpeg
-// http://localhost/image/#fii/width_200/test.jpeg -> IPX Error: Error: Modifiers are missing (/)
